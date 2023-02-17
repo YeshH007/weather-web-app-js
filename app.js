bc8eecb7079373edc2e4954cc1729d58da5d4601
YeshH007 <87498970+YeshH007@users.noreply.github.com>
2023-02-17 20:52:49 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,43 +1,43 @@
-let apikey="e58c42caa1f24325809135554231402" 
+let apikey = "e58c42caa1f24325809135554231402"
 
 
-let searchbutton=document.querySelector(".search")
-let searchtext=document.querySelector("#searchtext")
-let celsius=document.querySelector(".temp")
-let status1=document.querySelector(".status")
-let windspeed=document.querySelector("#speed")
-let humidity=document.querySelector("#humiditynum")
-async function getweather(searchval){
-    let getdata=fetch(`http://api.weatherapi.com/v1/current.json?key=e58c42caa1f24325809135554231402&q=${searchval}&aqi=no`)
-    let get=await getdata
-    let data=await get.json()
-    console.log(data)
-    if("error" in data){
-        document.querySelector(".error").style.display="block"
-        document.querySelector(".display").setAttribute("src","images\\pngegg (1).png")
-        celsius.innerHTML=`!` 
-        status1.textContent=`!`  
-        windspeed.textContent=`Windspeed !`
-        humidity.textContent=`Humidity !`
+let searchbutton = document.querySelector(".search")
+let searchtext = document.querySelector("#searchtext")
+let celsius = document.querySelector(".temp")
+let status1 = document.querySelector(".status")
+let windspeed = document.querySelector("#speed")
+let humidity = document.querySelector("#humiditynum")
+async function getweather(searchval) {
+    let getdata = fetch(`http://api.weatherapi.com/v1/current.json?key=e58c42caa1f24325809135554231402&q=${searchval}&aqi=no`)
+    let get = await getdata
+    let data = await get.json()
+
+    if ("error" in data) {
+        document.querySelector(".error").style.display = "block"
+        document.querySelector(".display").setAttribute("src", "images\\pngegg (1).png")
+        celsius.innerHTML = `!`
+        status1.textContent = `!`
+        windspeed.textContent = `Windspeed !`
+        humidity.textContent = `Humidity !`
     }
-    else{
-        document.querySelector(".error").style.display="none"
-        celsius.innerHTML=`${data.current.temp_c} &#8451 `    
-        document.querySelector(".display").setAttribute("src",data.current.condition.icon)
-        status1.textContent=`${data.current.condition.text}`  
-        windspeed.textContent=`Windspeed ${data.current.wind_kph}Kmph`
-        humidity.textContent=`Humidity ${data.current.humidity}%`
+    else {
+        document.querySelector(".error").style.display = "none"
+        celsius.innerHTML = `${data.current.temp_c} &#8451 `
+        document.querySelector(".display").setAttribute("src", data.current.condition.icon)
+        status1.textContent = `${data.current.condition.text}`
+        windspeed.textContent = `Windspeed ${data.current.wind_kph}Kmph`
+        humidity.textContent = `Humidity ${data.current.humidity}%`
     }
 }
 
-searchbutton.addEventListener("click",()=>{
-    let city=searchtext.value
+searchbutton.addEventListener("click", () => {
+    let city = searchtext.value
     getweather(city)
 })
-document.addEventListener("keypress",(event)=> {
+document.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
-        let city=searchtext.value
+        let city = searchtext.value
         getweather(city)
     }
-  });
+});
   
